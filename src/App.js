72e872d7a1c78729ef3883ccded5e1fd7e2d5868
Emre <emrekurtt1655@gmail.com>
2020-12-12 00:03:21 +0100

    add card component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React, {useEffect, useState} from 'react';
 
 import Navbar from './components/Navbar'
 import SearchBox from './components/SearchBox';
+import CardList from './components/Card';
 import axios from "axios";
 
 
-const options = {
-  method: 'GET',
-  url: 'https://covid-193.p.rapidapi.com/statistics',
-  params: {country: 'Andorra'},
-  headers: {
-    'x-rapidapi-key': '3b04f00e76msh8bf8189258b4a93p1a6a2fjsnfeb7d7a08448',
-    'x-rapidapi-host': 'covid-193.p.rapidapi.com'
-  }
-};
 
 
 
 
-function App() {
+
+function App(props) {
+  const [countryData, setCountryData] = useState([]);
+  const [countryName, setCountryName] = useState('Turkey')
+
+  const options = {
+    method: 'GET',
+    url: 'https://covid-193.p.rapidapi.com/statistics',
+    params: {country: `${countryName}`},
+    headers: {
+      'x-rapidapi-key': '3b04f00e76msh8bf8189258b4a93p1a6a2fjsnfeb7d7a08448',
+      'x-rapidapi-host': 'covid-193.p.rapidapi.com'
+    }
+  };
+
+
   const fetchData = () => {axios.request(options).then(function (response) {
+    setCountryData(response.data.response[0])
     console.log(response.data);
+    console.log(response.data.response[0].cases.total)
+    console.log(response.data.response[0].country)
   }).catch(function (error) {
     console.error(error);
   })};
@@ -31,6 +41,8 @@ function App() {
     <>
     <Navbar/>
     <SearchBox/>
+    <CardList item={countryData} />
+   
     </>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 
-const Card = () => {
-    return(
-        null
-    )
+export default function CardList({item}) {
+    const {cases, deaths} = item 
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {item.continent}-{item.country}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Population: {item.population}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Date: {item.day}
+        </Typography>
+        
+        <Typography variant="body2" component="p">
+          Cases 
+          <br />
+          Total: {cases?.total}
+          <br />
+          Recovered: {cases?.recovered}
+          <br />
+          New: {cases?.new}
+        </Typography>
+        <Typography variant="body2" component="p">
+          Deaths 
+          <br />
+          Total: {deaths?.total}
+          <br />
+          New: {deaths?.new}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
-
-
-export default Card;
