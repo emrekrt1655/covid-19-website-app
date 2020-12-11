49a262ca7cddb1aacc8b5fc654ea68a7bbb73d18
Emre <emrekurtt1655@gmail.com>
2020-12-11 21:17:47 +0100

    add component temp

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Card = () => {
+    return(
+        null
+    )
+}
+
+
+export default Card;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return(
+        null
+    )
+}
+
+
+export default Navbar;
