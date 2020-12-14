b5cad7c8ed1d3750f65128d2d3c58a093340d8bb
Emre <emrekurtt1655@gmail.com>
2020-12-14 21:22:13 +0100

    fix appjs

--- src/App.js
+++ src/App.js
@@ -1,49 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
 
-import Navbar from "./components/Navbar";
-import SearchBox from "./components/SearchBox";
-import CardList from "./components/Card";
-import Footer from "./components/Footer";
+import Navbar from './components/Navbar'
+import SearchBox from './components/SearchBox';
+import CardList from './components/Card';
 import axios from "axios";
+
+
+
+
+
+
 
 function App(props) {
   const [countryData, setCountryData] = useState([]);
-  const [countryName, setCountryName] = useState("Turkey");
+  const [countryName, setCountryName] = useState('Turkey')
 
   const options = {
-    method: "GET",
-    url: "https://covid-193.p.rapidapi.com/statistics",
-    params: { country: `${countryName}` },
+    method: 'GET',
+    url: 'https://covid-193.p.rapidapi.com/statistics',
+    params: {country: `${countryName}`},
     headers: {
-      "x-rapidapi-key": "3b04f00e76msh8bf8189258b4a93p1a6a2fjsnfeb7d7a08448",
-      "x-rapidapi-host": "covid-193.p.rapidapi.com",
-    },
+      'x-rapidapi-key': '3b04f00e76msh8bf8189258b4a93p1a6a2fjsnfeb7d7a08448',
+      'x-rapidapi-host': 'covid-193.p.rapidapi.com'
+    }
   };
+
 
   const fetchData = () => {
-    axios
-      .request(options)
-      .then(function (response) {
-        setCountryData(response.data.response[0]);
-        console.log(response.data);
-        console.log(response.data.response[0].cases.total);
-        console.log(response.data.response[0].country);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
+    axios.request(options)
+    .then(function (response) {
+    setCountryData(response.data.response[0])
+    console.log(response.data);
+    console.log(response.data.response[0].cases.total)
+    console.log(response.data.response[0].country)
+  }).catch(function (error) {
+    console.error(error);
+  })};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+  useEffect(()=>{fetchData()},[]);
+  
   return (
     <>
-      <Navbar />
-      <SearchBox />
-      <CardList item={countryData} />
-      <Footer />
+    <Navbar/>
+    <SearchBox/>
+    <CardList item={countryData} />
+   
     </>
   );
 }
